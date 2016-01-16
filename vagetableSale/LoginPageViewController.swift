5bcd02474471f4d1d6ccbe5dc0d88294d635b94e
wusu chiang <wusuchen@wusudeiMac.local>
2016-01-17 01:58:17 +0800

    0116

---
+++ vagetableSale/LoginPageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageViewController.swift
+//  Strawman
+//
+//  Created by wusuchen on 2016/1/14.
+//
+//
+
+import UIKit
+import FBSDKLoginKit
+
+class LoginPageViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
+    
+    
+
+}
---
+++ vagetableSale/MyOrderTableViewController.swift
@@ -0,0 +1,154 @@
+//
+//  MyOrderTableViewController.swift
+//  Strawman
+//
+//  Created by wusuchen on 2016/1/16.
+//
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class MyOrderTableViewController: UITableViewController {
+    
+    var userData = [Data]()
+    
+    
+    
+    
+
+    
+    func getDataFromServer(){
+        let userUrl = "http://139.162.37.39/api/v1"
+        let apiPath = userUrl + "/users"
+        
+        
+        Alamofire.request(.GET, apiPath,parameters: nil).responseJSON { response in switch response.result {
+        case .Success(let data):
+            
+            let result = JSON(data)["data"]
+
+        
+        for (_, subJson):(String, JSON) in result {
+            
+            let id = subJson["id"].stringValue
+            let name = subJson["name"].stringValue
+            let address = subJson["address"].stringValue
+            let mobil_num = subJson["mobil_num"].stringValue
+            let email = subJson["email"].stringValue
+            let user = Data(id: id, name: name, address: address, mobil_num: mobil_num, email: email)
+            
+            
+            self.userData.append(user)
+            print("\(user)")
+            
+            
+            
+        }
+        case .Failure(let error):
+            print(error)
+            
+            }
+
+        }
+        self.tableView.reloadData()
+
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getDataFromServer()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 3
+
+    }
+
+    @IBOutlet var dataTableView: UITableView!
+    
+
+    
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+
+ let cell = tableView.dequeueReusableCellWithIdentifier("cellStyle",forIndexPath: indexPath) as! CookTableViewCell
+       
+//        switch indexPath.section  {
+//        case 0:
+//            cell.vagetableNameLabel?.text = userData[indexPath.row]
+//        case 1:
+//            cell.vagetableNameLabel?.text = userData[indexPath.row]
+//        case 2:
+//            cell.vagetableNameLabel?.text = userData[indexPath.row]
+//        case 3:
+//            cell.vagetableNameLabel?.text = userData[indexPath.row]
+        
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
